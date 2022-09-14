6441e2a0cd4ac3fd5c90bb104d27c1a8439ac5f9
LeeDoyeun <113481666+LeeDoyeun@users.noreply.github.com>
2022-09-14 10:04:42 +0900

    Add files via upload
    9월 13일 자 수업내용

---
+++ 과제.java
@@ -0,0 +1,196 @@
+package com.human.ex;
+
+public class 과제 {
+	public static void main(String[] args) {
+		//1번 다음을 입력하면 어떻게 출력되는가?
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		
+		System.out.println(""); //2번 다음을 입력하면 어떻게 출력되는가?
+
+		System.out.print("1");
+		System.out.print("2");
+		System.out.print("3");
+
+		System.out.println(""); //3번 다음을 입력하면 어떻게 출력되는가?
+
+		System.out.print("hello");
+		System.out.println("World");
+
+		System.out.println(""); //4번 다음을 입력하면 어떻게 출력되는가?
+
+		System.out.print("12");
+		System.out.println("34");
+		System.out.println("56");
+
+		/*
+		 * System.out.print("12"); == p("12"); System.out.println("34");==pl("34");
+		 */
+
+		System.out.println(""); //5번 다음을 출력하려면 어떻게 입력해야 하는가?
+
+		System.out.println("hello");
+		System.out.println("world");
+
+		System.out.println(""); // 6번 . 다음은 p(“1”);과 p(“ ”);pl(“”);p(“2”);p(“3”); 만으로 출력을 완성하자.
+
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.print("1");
+		System.out.println("");
+		System.out.print(" ");
+		System.out.print("1");
+		System.out.print("2");
+		System.out.println("");
+		System.out.print("1");
+		System.out.print("2");
+		System.out.print("3");
+
+		System.out.println("");//7번 다음은 p(“*”);과 pl(“”); 만으로 출력을 완성하자.
+
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+
+		System.out.println("");//8번 다음은 p(“*”);과 pl(“”); 만으로 출력을 완성하자.
+
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+
+		System.out.println("");//9번 다음은 p(“*”);p(“ ”);pl(“”); 만으로 출력을 완성하자.
+
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+
+		System.out.println("");//10번 다음은 p(“*”);p(“ ”);pl(“”); 만으로 출력을 완성하자.
+
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.print("*");
+		
+		System.out.println("");//연습문제 3 화면에 '안녕자바'를 출력해보자
+		
+		System.out.print("'안녕자바'");
+		
+		System.out.println("");//연습문제 4 본인의 정보를 출력하는 프로그램을 만들어 보자. 이름 이메일 전화번호등 명암으로 만들어 출력해보자.
+		
+		System.out.println("이름 : 이도연");
+		System.out.println("이메일 : ldy14032@naver.com");
+		System.out.println("전화번호 : 01091401827");
+		
+		System.out.println("");//연습문제 5다음 각종 삼각형 사각형 모양을 화면에 출력해보자 -1
+		
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		
+		System.out.println("");//연습문제 5-2
+		
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+		
+		System.out.println("");//연습문제 5-3
+		
+		System.out.println("    *");
+		System.out.println("   **");
+		System.out.println("  ***");
+		System.out.println(" ****");
+		System.out.println("*****");
+		
+		System.out.println("");//연습문제 6 다음 성적 표를 콘솔에 출력하시오.
+		
+		System.out.println("******************");
+		System.out.println("*\t성적표\t *");
+		System.out.println("******************");
+		System.out.println("* 국어\t50\t *");
+		System.out.println("******************");
+		System.out.println("* 영어\t50\t *");
+		System.out.println("******************");
+		System.out.println("* 수학\t50\t *");
+		System.out.println("******************");
+		System.out.println("* 컴퓨터학과   홍길동*");
+		System.out.println("******************");
+		
+		System.out.println("");//연습문제 7 다음과 같은 탈력을 \t를 이용해서 오늘 날짜 월로 만들어 보자.
+		
+		System.out.println("일\t월\t화\t수\t목\t금\t토");
+		System.out.println("1\t2\t3\t4\t5\t6\t7");
+		System.out.println("8\t9\t10\t11\t12\t13\t14");
+		System.out.println("15\t16\t17\t18\t19\t20\t21");
+		System.out.println("22\t23\t24\t25\t26\t27\t28");
+		System.out.println("29\t30\t31");
+		
+
+	}
+
+}
